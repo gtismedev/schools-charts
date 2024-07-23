14b9db7785f9c0ebbe76a2226610b0e170f27b29
FellipeMiguel <fellipemiguel1@icloud.com>
2024-07-23 18:15:10 +0200

    feat: create routes and error page

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./routes/root";
-import ErrorPage from "./error-page";
+
+import Root from "./routes/index/root";
+import School from "./routes/school/school";
+import ErrorPage from "./routes/error/error-page";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: "/escola/:schoolId",
+    element: <School />,
   },
 ]);
 
---
+++ src/routes/school/school.jsx
@@ -0,0 +1,12 @@
+import { Header } from "../../components/header/header";
+
+function School() {
+  return (
+    <>
+      <Header />
+      <div>Schools page</div>
+    </>
+  );
+}
+
+export default School;
